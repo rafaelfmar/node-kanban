ff136f32afd44bc5bdd3ae04116da9b26b4c5ae6
rafaelfmar <rafael.fmarques@outlook.com>
2019-10-19 14:58:00 -0300

    Mongodb connection with mongoose

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/node-kanban', {
+  useNewUrlParser: true
+});
 
 app.get('/', (req, res) => {
   res.send('Hello node');
